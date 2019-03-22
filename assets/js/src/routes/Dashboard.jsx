79262f63be7fb3bb8e5523d0dff4e907cd0dea61
drg <drg@drgs-MacBook-Pro.local>
2019-03-21 23:33:15 -0400

    remove unused component

--- assets/js/src/routes/Dashboard.jsx
+++ assets/js/src/routes/Dashboard.jsx
@@ -2,7 +2,6 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { deleteTask } from '../../api';
-import { Tooltip } from 'react-tippy';
 
 const mapStateToProps = ({tasks, token}) => ({
   tasks,
